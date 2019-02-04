b45fbea0e2bf91bed51b717f1fc0c109c1d3e7ff
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-02-04 13:53:46 -0500

    final wordcount exercise

--- 17_FileIO_Reading_in/file-io-part1-exercises-pair/Program.cs
+++ 17_FileIO_Reading_in/file-io-part1-exercises-pair/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace file_io_part1_exercises_pair
 {
@@ -6,7 +7,54 @@ namespace file_io_part1_exercises_pair
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ask for a filesystem path for a text file ('alices_adventures_in_wonderland.txt')
+            // read the contents of the file
+            // display the number of words
+            // display the number of sentences
+
+            Console.WriteLine("Enter the full file path to read:");
+            string directory = Console.ReadLine();
+
+            int wordCount = 0;
+            int sentenceCount = 0;
+            string[] wordSeparators = {" ", ". ", "! ", "? ", ", "};
+            string[] words;
+            string readInLine = "";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(directory))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        //read in line
+                        readInLine = sr.ReadLine();
+
+                        //word counting
+                        words = readInLine.Split(wordSeparators, StringSplitOptions.None);
+                        wordCount += words.Length;
+
+                        //sentence counting
+                        foreach (char letter in readInLine)
+                        {
+                            if (letter == '.' ||
+                                letter == '!' ||
+                                letter == '?')
+                            {
+                                sentenceCount++;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error reading the file");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine($"Word count = {wordCount}\nSentence Count = {sentenceCount}");
+            Console.ReadLine();
         }
     }
 }
